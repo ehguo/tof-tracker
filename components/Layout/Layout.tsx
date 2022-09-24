0eb5d309af1c46548b2bd8eda7fdca93e40618f3
Edward Guo <eguo@pathwaylabs.io>
2022-09-23 20:39:18 -0700

    dependencies + scaffolding (#1)
    * nextjs
    
    * scaffolding

---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,11 @@
+import { Container } from '@mui/material';
+
+const Layout = () => {
+  return (
+    <Container>
+      
+    </Container>
+  );
+};
+
+export default Layout;
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from '@mui/material';
+
+const Home = () => {
+  return (
+    <Container>
+      Scaffolding
+    </Container>
+  );
+}
+
+export default Home;
